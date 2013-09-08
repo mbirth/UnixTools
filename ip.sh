41dd2f072ceb87931b16f0cc580e706b859719df
Markus Birth <mbirth@gmail.com>
2013-09-09 00:47:02 +0200

    Fixed ip.sh

--- ip.sh
+++ ip.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-IP1=`wget http://media.silversolutions.de/webco/ip.php -O - -q --no-cache`
+#IP1=`wget http://media.silversolutions.de/webco/ip.php -O - -q --no-cache`
 IP2=`wget http://admin.birth-online.de/ip.php -O - -q --no-cache`
- echo "$IP1" | grep REMOTE_ADDR | cut -c 22-
- echo "$IP2"
+#echo "$IP1" | grep REMOTE_ADDR | cut -c 22-
+echo "$IP2"
