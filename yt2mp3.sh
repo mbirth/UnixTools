b26fdc1be4cb518e9e9e6395b006335e37b20929
Markus Birth <mbirth@gmail.com>
2014-05-10 22:57:59 +0200

    Added script to download YouTube audio and convert to MP3.

---
+++ yt2mp3.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ "$#" = "0" ]; then
+    echo "Usage: $0 youtube-url output-filename"
+    exit 1
+fi
+
+youtube-dl "$1" -o - | avconv -i pipe:0 -f wav -vn - | lame -m j --replaygain-fast --vbr-new -V 2 -o --id3v2-only --resample 44.1 - "$2.mp3"
