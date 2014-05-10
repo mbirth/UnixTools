6b3da7e0b3f01ada377f6f301a8e032e755cf67d
Markus Birth <mbirth@gmail.com>
2014-05-10 22:55:43 +0200

    Get fish version number from FISH-BUILD-VERSION file.

--- checkinst.sh
+++ checkinst.sh
@@ -3,6 +3,7 @@
 
 PKG_NAME=`cat configure | grep "PACKAGE_NAME=" | sed -r "s/^.*=['\"]?([^'\"]*)['\"]?$/\1/g"`
 PKG_VERSION=`cat configure | grep "PACKAGE_VERSION=" | sed -r "s/^.*=['\"]?([^'\"]*)['\"]?$/\1/g"`
+PKG_VERSION=`cat FISH-BUILD-VERSION-FILE | grep "FISH_BUILD_VERSION =" | sed -r "s/^.* = ['\"]?([^'\"]*)['\"]?$/\1/g"`
 PKG_OLDCODE=`ls -1vr ./${PKG_NAME}_${PKG_VERSION}* | head -n 1 | sed -r "s/^.*${PKG_NAME}_${PKG_VERSION}-(.*)_.*$/\1/g"`
 PKG_CODE=`expr $PKG_OLDCODE + 1`
 SPEC_FILE=`ls -1r ./*.spec`
