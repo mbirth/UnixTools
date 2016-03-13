441685c44d8e6ba5898623509dab7e4b7ad43fb7
Markus Birth <mbirth@gmail.com>
2016-03-14 00:25:25 +0100

    Added downmix of 5.1 to stereo.

--- plexoptimize.sh
+++ plexoptimize.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ffmpeg -i "$1" -c:v h264 -c:a libvo_aacenc -preset fast -movflags +faststart -crf 18 -b:a 192k "$1.mp4"
+ffmpeg -i "$1" -c:v h264 -c:a libvo_aacenc -preset fast -movflags +faststart -crf 18 -b:a 192k -ac 2 "$1.mp4"
