5c67cb04147229b13839019f78fec7489e293ece
Markus Birth <mbirth@gmail.com>
2016-12-18 23:46:34 +0100

    2 more converter scripts.

---
+++ mkv2mp4.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "Syntax: $0 VIDEOFILE"
+    exit 1
+fi
+
+while [ -n "$1" ]; do
+
+    # Quality: -crf 18 = almost lossless, -crf 22 = medium, -crf 28 = low
+    # Deinterlace: -vf yadif=1 -field-dominance 0/1 (0=Top field first, 1=Bottom field first; doubles framerate)
+    #           or -vf yadif=1:0 (or 1:1)
+    #              -vf yadif=0 (calculates 1 frame from both fields; keeps framerate)
+    # Scale: -vf scale=-1:720
+    ffmpeg -i "$1" -c:v h264 -c:a aac -c:s copy -preset fast -movflags +faststart -crf 18 -b:a 160k -ar 44100 -ac 2 "${1}.mp4"
+    shift
+done
