d93201f105cb3d0c3f8855024536adff1bd109e1
Markus Birth <mbirth@gmail.com>
2017-02-06 22:05:52 +0100

    Get audio only for converting to mp3.

--- yt2mp3.sh
+++ yt2mp3.sh
@@ -4,4 +4,4 @@ if [ "$#" = "0" ]; then
     exit 1
 fi
 
-youtube-dl --ignore-config "$1" -o - | ffmpeg -i pipe:0 -f wav -vn - | lame -m j --replaygain-fast --vbr-new -V 2 -o --id3v2-only --resample 44.1 - "$2.mp3"
+youtube-dl --ignore-config -f bestaudio "$1" -o - | ffmpeg -i pipe:0 -f wav -vn - | lame -m j --replaygain-fast --vbr-new -V 2 -o --id3v2-only --resample 44.1 - "$2.mp3"
