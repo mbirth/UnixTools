7df722df665132f6e446ceb6b459e7219decc17f
Markus Birth <mbirth@gmail.com>
2017-12-05 02:28:23 +0100

    More optimisations.

--- plexoptimize.sh
+++ plexoptimize.sh
@@ -15,6 +15,9 @@ fi
 #           or -vf yadif=1:0 (or 1:1)
 #              -vf yadif=0 (calculates 1 frame from both fields; keeps framerate)
 # Scale: -vf scale=-1:720 (or -2 for values dividable by 2)
+# Scale to Aspect: -filter:v scale=iw*sar:ih,setsar=1:1
+# Set audio language: -metadata:s:a:0 language=eng
 #ffmpeg -i "$1" "${SUBPARAMS[@]}" -c:v h264 -c:a libvo_aacenc -preset fast -movflags +faststart -crf 18 -b:a 192k -ac 2 "$1.mp4"
-#ffmpeg -i "$1" "${SUBPARAMS[@]}" -c:v h264 -c:a aac -preset fast -movflags +faststart -vf yadif=1:0,scale=-1:720 -crf 18 -b:a 160k -ar 44100 -ac 2 "$1.mp4"
-ffmpeg -i "$1" "${SUBPARAMS[@]}" -c:v h264 -c:a aac -preset fast -movflags +faststart -crf 18 -b:a 160k -ar 44100 -ac 2 "$1.mp4"
+#ffmpeg -i "$1" "${SUBPARAMS[@]}" -c:v h264 -c:a aac -preset fast -movflags +faststart -vf yadif=0,scale=-2:720 -crf 18 -b:a 160k -ar 44100 -ac 2 "$1.mp4"
+ffmpeg -i "$1" "${SUBPARAMS[@]}" -c:v h264 -c:a aac -preset fast -movflags +faststart -vf scale=-2:720 -crf 20 -b:a 160k -ar 44100 -ac 2 "$1.mp4"
+#ffmpeg -i "$1" "${SUBPARAMS[@]}" -c:v h264 -c:a aac -preset fast -movflags +faststart -crf 18 -b:a 160k -ar 44100 -ac 2 "$1.mp4"
