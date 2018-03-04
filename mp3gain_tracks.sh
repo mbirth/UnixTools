7213aa7139c1858fbd4147a40ed38e90e36a605a
Markus Birth <mbirth@gmail.com>
2018-03-04 18:18:58 +0100

    Normalise MP3s to 93dB instead of default 89dB.

--- mp3gain_tracks.sh
+++ mp3gain_tracks.sh
@@ -11,7 +11,13 @@ if [ -z "$CONCURRENCY_LEVEL" ]; then
 fi
 
 echo "Processing using $CONCURRENCY_LEVEL processes..."
-nice -n2 find "$@" -print0 | xargs -0 -P $CONCURRENCY_LEVEL -n 1 -I {} mp3gain -q -k -p -r -s i "{}"
+#  -q - Quiet mode: no status messages
+#  -k - automatically lower Track/Album gain to not clip audio
+#  -p - Preserve original file timestamp
+#  -r - apply Track gain automatically (all files set to equal loudness)
+#  -s i - use ID3v2 tag for MP3 gain info
+#  -d <n> - modify suggested dB gain (89dB) by floating-point n
+nice -n2 find "$@" -print0 | xargs -0 -P $CONCURRENCY_LEVEL -n 1 -I {} mp3gain -q -k -p -r -s i -d 4.0 "{}"
 #nice -n2 find "$@" -print0 | xargs -0 -P $CONCURRENCY_LEVEL -n 1 -I {} replaygain --no-album "{}"
 
 echo "All done."
